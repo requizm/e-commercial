4cc7874bfced55a93cb688b84003e0a9228565fc
requizm <mehmet41650@gmail.com>
2022-01-29 23:39:54 +0300

    #5 Gereksizler silindi ve route ayarlandı

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import './index.scss';
-import App from './component/App/App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./page/LoginPage/LoginPage";
+import { RegisterPage } from "./page/RegisterPage/RegisterPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />}>
+        </Route>
+        <Route path="/register" element={<RegisterPage />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +24,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
