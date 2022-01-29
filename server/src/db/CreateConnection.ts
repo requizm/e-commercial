3029e4457f12b4720abd07653b680c642948f637
requizm <mehmet41650@gmail.com>
2022-01-29 23:30:39 +0300

    #5 user sınıfı oluşturuldu

--- server/src/db/CreateConnection.ts
+++ server/src/db/CreateConnection.ts
@@ -1,11 +1,12 @@
 import { Connection, createConnection } from 'typeorm';
+import { User } from './entity/User';
 
 export async function initializeConnection(): Promise<Connection> {
   return createConnection({
     name: 'default',
     type: 'sqlite',
     database: 'test.sqlite',
-    entities: [],
+    entities: [User],
     synchronize: true,
     logging: true,
   });
