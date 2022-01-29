0841adbdfda68f5f02127a991e5bd9558a06b1a5
requizm <mehmet41650@gmail.com>
2022-01-29 23:36:01 +0300

    #5 Login sayfası oluşturuldu

---
+++ client/src/page/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../../component/Header/Header';
+import { Login } from '../../component/Login/Login';
+import './style/LoginPage.scss';
+
+export function LoginPage() {
+  return (
+    <div className="App">
+      <Header />
+      <Login />
+    </div>
+  );
+}
