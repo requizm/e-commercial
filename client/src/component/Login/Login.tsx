0c01792fc97fc9c0f6d07a42a12e527363424011
requizm <mehmet41650@gmail.com>
2022-01-31 00:19:22 +0300

    #12 Class component'ler function'a çevrildi

--- client/src/component/Login/Login.tsx
+++ client/src/component/Login/Login.tsx
@@ -1,27 +1,22 @@
-import { ChangeEvent, Component, FormEvent } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Post } from '../../util/ApiCall';
 import './style/Login.scss';
 
-export class Login extends Component<any, any> {
-    constructor(prop: any) {
-        super(prop);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.state = { email: '', password: '', formSubmit: null };
+export function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [formSubmit, setFormSubmit] = useState('');
+    let navigate = useNavigate();
+
+    function handleValidation(): boolean {
+        return !email || !password;
     }
 
-    handleValidation(): boolean {
-        if (this.state.email && this.state.password) {
-            return true;
-        }
-        return false;
-    }
-
-    async handleChange(event: ChangeEvent<HTMLInputElement>) {
-        await this.setState({ [event.target.name]: event.target.value });
+    async function handleChange(setEvent : any, event: ChangeEvent<HTMLInputElement>) {
+        setEvent(event.target.value);
         let object = document.getElementById('login-button');
-        if (!this.handleValidation()) {
+        if (handleValidation()) {
             document.getElementById("login-button")?.setAttribute("disabled", "disabled");
             object?.classList.remove("active-btn");
             object?.classList.add("disable-btn");
@@ -33,44 +28,45 @@ export class Login extends Component<any, any> {
         }
     }
 
-    async handleSubmit(event: FormEvent<HTMLFormElement>) {
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        if (this.handleValidation()) {
-            const data = { email: this.state.email, password: this.state.password };
+        if (!handleValidation()) {
+            const data = { email: email, password: password };
             const response = await Post(JSON.stringify(data), "auth/login");
-            this.setState({ formSubmit: response.ok });
+            if (response.ok) {
+                navigate("../");
+            }
+            const responseText = await response.text();
+            setFormSubmit(responseText);
         }
         else {
-            this.setState({ formSubmit: null });
+            setFormSubmit('');
         }
     }
-
-    render() {
-        if (this.state.formSubmit) {
-            return <Navigate to="/" />;
-        }
-        return <div className="login-card">
-            <form onSubmit={this.handleSubmit}>
-                <div className="login-card-form">
-                    <div className="input-block email-block">
-                        <div className="email-text">E-mail</div>
-                        <input type="text" name="email" placeholder="Enter" className="text-input email-input" autoComplete="off" value={this.state.email} onChange={this.handleChange} />
-                    </div>
-                    <div className="input-block password-block">
-                        <div className="password-text">Password</div>
-                        <input type="password" name="password" placeholder="Enter" className="text-input password-input" autoComplete="off" value={this.state.password} onChange={this.handleChange} />
-                    </div>
+    return <div className="login-card">
+        <form onSubmit={handleSubmit}>
+            <div className="login-card-form">
+                <div className="input-block email-block">
+                    <div className="email-text">E-mail</div>
+                    <input type="text" name="email" placeholder="Enter" className="text-input email-input" autoComplete="off" onChange={e => handleChange(setEmail, e)} />
                 </div>
-                <div className="login-card-bottom">
-                    <div className="login-button-block">
-                        <input type="submit" id="login-button" disabled={true} className="btn disable-btn login-button" value="Login" />
-                    </div>
-                    <div className="register-block">
-                        <span className="register-text">Don't have an account?</span>
-                        <Link className="register-link" to="/register">Register</Link>
-                    </div>
+                <div className="input-block password-block">
+                    <div className="password-text">Password</div>
+                    <input type="password" name="password" placeholder="Enter" className="text-input password-input" autoComplete="off" onChange={e => handleChange(setPassword, e)} />
                 </div>
-            </form>
-        </div>
-    }
+                <div className="status">
+                    <div className="status-text">{formSubmit}</div>
+                </div>
+            </div>
+            <div className="login-card-bottom">
+                <div className="login-button-block">
+                    <input type="submit" id="login-button" disabled={true} className="btn disable-btn login-button" value="Login" />
+                </div>
+                <div className="register-block">
+                    <span className="register-text">Don't have an account?</span>
+                    <Link className="register-link" to="/register">Register</Link>
+                </div>
+            </div>
+        </form>
+    </div>
 }
--- client/src/component/Register/Register.tsx
+++ client/src/component/Register/Register.tsx
@@ -1,114 +1,129 @@
-import { ChangeEvent, Component, FormEvent } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { Post } from '../../util/ApiCall';
 import './style/Register.scss';
 
-export class Register extends Component<any, any> {
-    constructor(prop: any) {
-        super(prop);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.state = {
-            firstName: '', lastName: '', email: '', password: '',
-            firstNameError: '', lastNameError: '', emailError: '', passwordError: '', formSubmit: null
-        };
+export function Register() {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstNameError, setFirstNameError] = useState('');
+    const [lastNameError, setLastNameError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [formSubmit, setFormSubmit] = useState('');
+
+    let navigate = useNavigate();
+
+    function handleValidation(): boolean {
+        return !firstName || !lastName || !email || !password;
     }
 
-    handleValidation(): boolean {
-        let error = false;
-        if (!this.state.firstName) {
-            this.setState({ firstNameError: "First name is required" });
-            error = true;
+    function updateValidationErrors() {
+
+        if (!firstName) {
+            setFirstNameError("First name is required");
         }
         else {
-            this.setState({ firstNameError: "" });
+            setFirstNameError("");
         }
 
-        if (!this.state.lastName) {
-            this.setState({ lastNameError: "Last name is required" });
-            error = true;
+        if (!lastName) {
+            setLastNameError("Last name is required");
         }
         else {
-            this.setState({ lastNameError: "" });
+            setLastNameError("");
         }
 
 
-        if (!this.state.email) {
-            this.setState({ emailError: "Email is required" });
-            error = true;
+        if (!email) {
+            setEmailError("Email is required");
         }
         else {
-            this.setState({ emailError: "" });
+            setEmailError("");
         }
 
-        if (!this.state.password) {
-            this.setState({ passwordError: "Password is required" });
-            error = true;
+        if (!password) {
+            setPasswordError("Password is required");
         }
         else {
-            this.setState({ passwordError: "" });
+            setPasswordError("");
         }
-
-        return !error;
     }
 
-    handleChange(event: ChangeEvent<HTMLInputElement>) {
-        this.setState({ [event.target.name]: event.target.value });
+    async function handleChange(setEvent: any, event: ChangeEvent<HTMLInputElement>, errorText: string, errorSetter: any) {
+        setEvent(event.target.value);
+        if (errorText && event.target.value) {
+            errorSetter('')
+        }
+
+        let object = document.getElementById('register-button');
+        if (!handleValidation()) {
+            document.getElementById("register-button")?.setAttribute("disabled", "disabled");
+            object?.classList.remove("active-btn");
+            object?.classList.add("disable-btn");
+        }
+        else {
+            document.getElementById("register-button")?.removeAttribute("disabled");
+            object?.classList.remove("disable-btn");
+            object?.classList.add("active-btn");
+        }
     }
 
-    async handleSubmit(event: FormEvent<HTMLFormElement>) {
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        if (this.handleValidation()) {
-            const data = { firstName: this.state.firstName, lastName: this.state.lastName, email: this.state.email, password: this.state.password };
+        updateValidationErrors();
+        if (!handleValidation()) {
+            const data = { firstName: firstName, lastName: lastName, email: email, password: password };
             const response = await Post(JSON.stringify(data), "auth/register");
-            this.setState({ formSubmit: response.ok });
+            if (response.ok) {
+                navigate("../login");
+            }
+            const responseText = await response.text();
+            setFormSubmit(responseText);
         }
         else {
-            this.setState({ formSubmit: null });
+            setFormSubmit('');
         }
     }
-
-    render() {
-        if (this.state.formSubmit) {
-            return <Navigate to='/login' />
-        }
-        return <div className="register-card">
-            <form onSubmit={this.handleSubmit}>
-                <div className="register-card-form">
-                    <div className="input-block firstName-block">
-                        <div className="firstName-text">Name*</div>
-                        <input type="text" placeholder="Enter" name="firstName" className="text-input firstName-input" autoComplete="off" value={this.state.firstName} onChange={this.handleChange} />
-                        <div className="error-text">{this.state.firstNameError}</div>
-                    </div>
-                    <div className="input-block lastName-block">
-                        <div className="lastName-text">Surname*</div>
-                        <input type="text" placeholder="Enter" name="lastName" className="text-input lastName-input" autoComplete="off" value={this.state.lastName} onChange={this.handleChange} />
-                        <div className="error-text">{this.state.lastNameError}</div>
-                    </div>
-                    <div className="input-block email-block">
-                        <div className="email-text">Email*</div>
-                        <input type="text" placeholder="Enter" name="email" className="text-input email-input" autoComplete="off" value={this.state.email} onChange={this.handleChange} />
-                        <div className="error-text">{this.state.emailError}</div>
-                    </div>
-                    <div className="input-block password-block">
-                        <div className="password-text">Password*</div>
-                        <input type="password" placeholder="Enter" name="password" className="text-input password-input" autoComplete="off" value={this.state.password} onChange={this.handleChange} />
-                        <div className="error-text">{this.state.passwordError}</div>
-                    </div>
-                    <div className="status">
-                        <div className="status-text">{this.state.formSubmit !== null ? (this.state.formSubmit ? "Success" : "Failed") : ""}</div>
-                    </div>
+    return <div className="register-card">
+        <form onSubmit={handleSubmit}>
+            <div className="register-card-form">
+                <div className="input-block firstName-block">
+                    <div className="firstName-text">Name*</div>
+                    <input type="text" placeholder="Enter" name="firstName" className="text-input firstName-input" autoComplete="off" onChange={e => handleChange(setFirstName, e, firstNameError, setFirstNameError)} />
+                    <div className="error-text">{firstNameError}</div>
                 </div>
-                <div className="register-card-bottom">
-                    <div className="register-button-block">
-                        <input type="submit" className="btn active-btn register-button" value="Register" />
-                    </div>
-                    <div className="login-block">
-                        <span className="login-text">Do you have a account?</span>
-                        <Link className="login-link" to="/login">Login</Link>
-                    </div>
+                <div className="input-block lastName-block">
+                    <div className="lastName-text">Surname*</div>
+                    <input type="text" placeholder="Enter" name="lastName" className="text-input lastName-input" autoComplete="off" onChange={e => handleChange(setLastName, e, lastNameError, setLastNameError)} />
+                    <div className="error-text">{lastNameError}</div>
                 </div>
-            </form>
-        </div>
-    }
+                <div className="input-block email-block">
+                    <div className="email-text">Email*</div>
+                    <input type="text" placeholder="Enter" name="email" className="text-input email-input" autoComplete="off" onChange={e => handleChange(setEmail, e, emailError, setEmailError)} />
+                    <div className="error-text">{emailError}</div>
+                </div>
+                <div className="input-block password-block">
+                    <div className="password-text">Password*</div>
+                    <input type="password" placeholder="Enter" name="password" className="text-input password-input" autoComplete="off" onChange={e => handleChange(setPassword, e, passwordError, setPasswordError)} />
+                    <div className="error-text">{passwordError}</div>
+                </div>
+                <div className="status">
+                    <div className="status-text">{formSubmit}</div>
+                </div>
+            </div>
+            <div className="register-card-bottom">
+                <div className="register-button-block">
+                    <input type="submit" className="btn active-btn register-button" value="Register" />
+                </div>
+                <div className="login-block">
+                    <span className="login-text">Do you have a account?</span>
+                    <Link className="login-link" to="/login">Login</Link>
+                </div>
+            </div>
+        </form>
+    </div>
 }
