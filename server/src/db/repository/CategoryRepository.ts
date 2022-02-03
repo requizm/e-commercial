09cc91b937630b038b43fdd5e85f9a2e1d0f13de
requizm <mehmet41650@gmail.com>
2022-02-03 22:04:18 +0300

    #11 Repo'lar oluşturuldu

---
+++ server/src/db/repository/CategoryRepository.ts
@@ -0,0 +1,28 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Category } from '../entity/Category';
+
+@EntityRepository(Category)
+export class CategoryRepository extends Repository<Category> {
+  async findByName(name: string) {
+    return await this.findOne({ name: name });
+  }
+
+  async getAll(): Promise<Category[]> {
+    return await this.find({ relations: ['parent'] });
+  }
+
+  async getById(id: number): Promise<Category> {
+    return await this.findOne({
+      where: { id: id },
+      relations: ['parent'],
+    });
+  }
+
+  async getChildren(category: Category) {
+    const children = await this.find({
+      where: { parent: category },
+      relations: ['parent'],
+    });
+    return children;
+  }
+}
