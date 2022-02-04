04b4b77c40e428f79980a1acc01f0a08bf930829
requizm <mehmet41650@gmail.com>
2022-02-05 02:41:49 +0300

    #15 Sayfalara sidebar eklendi

--- client/src/page/Admin/Product/ProductListPage/ProductListPage.tsx
+++ client/src/page/Admin/Product/ProductListPage/ProductListPage.tsx
@@ -1,13 +1,21 @@
-import { Header } from '../../../../component/Header/Header';
-import { ProductList } from '../../../../component/Admin/Product/ProductList/ProductList';
+import '../../Admin.scss';
 
-import './style/ProductListPage.scss';
+import { ProductList } from '../../../../component/Admin/Product/ProductList/ProductList';
+import { Sidebar } from '../../../../component/Admin/Sidebar/Sidebar';
+import { Header } from '../../../../component/Header/Header';
 
 export function ProductListPage() {
     return (
-        <div className="App">
+        <div className="app">
             <Header />
-            <ProductList />
+            <div className="content">
+                <div className="left">
+                    <Sidebar />
+                </div>
+                <div className="right">
+                    <ProductList />
+                </div>
+            </div >
         </div>
     );
 }
